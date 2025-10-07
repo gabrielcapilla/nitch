84760831ee9fd65a1c978cd98b308830733abcce
gabrielcapilla <gabrielcapilla@users.noreply.github.com>
2025-10-07 20:09:39 +0200

    refactor(pkgs): add error handling to package count functions

--- src/funcs/packages/getDpkgPkgs.nim
+++ src/funcs/packages/getDpkgPkgs.nim
@@ -1,8 +1,9 @@
-import
-  std/[strutils, osproc]
+import std/[osproc, strutils]
 
 proc getDpkgPkgs*(): string =
-  let
-    count: string = osproc.execCmdEx("dpkg -l")[0]
-
-  result = $(count.split("\n").len - 1)
+  try:
+    let count: string = osproc.execCmdEx("dpkg -l")[0]
+    let lineCount: int = count.split("\n").len - 1
+    result = $lineCount
+  except OSError, IOError:
+    result = "0"
