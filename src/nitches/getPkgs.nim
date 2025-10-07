57e96d3024707b2bc92b1e68109204f59420579e
gabrielcapilla <gabrielcapilla@users.noreply.github.com>
2025-10-07 20:09:49 +0200

    refactor(getPkgs): update import order and organization

--- src/nitches/getPkgs.nim
+++ src/nitches/getPkgs.nim
@@ -1,10 +1,18 @@
-import ../funcs/packages/[getXbpsPkgs, getRpmPkgs, getPortagePkgs, getDpkgPkgs, getPacmanPkgs]
+import
+  ../funcs/packages/
+    [getXbpsPkgs, getRpmPkgs, getPortagePkgs, getDpkgPkgs, getPacmanPkgs]
 
 proc getPkgs*(distroId: string): string =
-  case distroId:
-  of "void": getXbpsPkgs()
-  of "fedora": getRpmPkgs()
-  of "gentoo": getPortagePkgs()
-  of "debian", "pop", "ubuntu": getDpkgPkgs()
-  of "arch", "archcraft", "manjaro", "cachyos": getPacmanPkgs()
-  else: ">3"
+  case distroId
+  of "void":
+    getXbpsPkgs()
+  of "fedora":
+    getRpmPkgs()
+  of "gentoo":
+    getPortagePkgs()
+  of "debian", "pop", "ubuntu":
+    getDpkgPkgs()
+  of "arch", "archcraft", "manjaro", "cachyos":
+    getPacmanPkgs()
+  else:
+    ">3"
