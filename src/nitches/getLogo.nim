49a1b44ac31cc6ae973eb52716d2638d6c1a21b9
gabrielcapilla <gabrielcapilla@users.noreply.github.com>
2025-10-07 20:09:15 +0200

    refactor(getLogo): replace case statement with table lookup for better performance

--- src/nitches/getLogo.nim
+++ src/nitches/getLogo.nim
@@ -1,22 +1,25 @@
+import std/tables
 import ../assets/logos
 
+const logoTable: Table[string, string] = {
+  "arch": archLogo,
+  "artix": artixLogo,
+  "cachyos": cachyosLogo,
+  "centos": centosLogo,
+  "debian": debianLogo,
+  "endeavouros": endeavourosLogo,
+  "fedora": fedoraLogo,
+  "gentoo": gentooLogo,
+  "manjaro": manjaroLogo,
+  "linuxmint": mintLogo,
+  "opensuse": opensuseLogo,
+  "pop": poposLogo,
+  "redhat": redhatLogo,
+  "slackware": slackwareLogo,
+  "ubuntu": ubuntuLogo,
+  "void": voidLogo,
+  "Zorin OS": zorinLogo,
+}.toTable
+
 func getLogo*(distroId: string): string =
-  case distroId:
-  of "arch": archLogo
-  of "artix": artixLogo
-  of "cachyos": cachyosLogo
-  of "centos": centosLogo
-  of "debian": debianLogo
-  of "endeavouros": endeavourosLogo
-  of "fedora": fedoraLogo
-  of "gentoo": gentooLogo
-  of "manjaro": manjaroLogo
-  of "linuxmint": mintLogo
-  of "opensuse": opensuseLogo
-  of "pop": poposLogo
-  of "redhat": redhatLogo
-  of "slackware": slackwareLogo
-  of "ubuntu": ubuntuLogo
-  of "void": voidLogo
-  of "Zorin OS": zorinLogo
-  else: nitchLogo
+  result = logoTable.getOrDefault(distroId, nitchLogo)
