917815d9c845436194044ba5e39829ecdd605f88
gabrielcapilla <gabrielcapilla@users.noreply.github.com>
2025-10-07 20:08:46 +0200

    feat(assets): update version to 0.2.0 and add nologo flag to help message

--- src/assets/assets.nim
+++ src/assets/assets.nim
@@ -1,11 +1,11 @@
 const
-  programVersion*: string = "nitch - 0.1.8\n"
-  helpMsg*: string = """
-
+  programVersion*: string = "nitch - 0.2.0\n"
+  helpMsg*: string =
+    """
  nitch - incredibly fast system fetch written in nim
 
   -f --fetch   | return fetch about system
+  -n --nologo  | return fetch without logo
   -h --help    | return help message
   -v --version | return version of program
-
 """
